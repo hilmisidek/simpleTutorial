7f4e0614cb455f9fca748dd43b0a196c0634403e
hilmisidek <khairulhilmi.sidek@petronas.com>
2024-06-07 15:41:43 +0800

    one

---
+++ src/AboutUs.js
@@ -0,0 +1,16 @@
+// src/AboutUs.js
+import React from 'react';
+import './App.css';
+
+function AboutUs() {
+  return (
+    <div className="AboutUs">
+      <header className="App-header">
+        <h1>About Us</h1>
+        <p>Welcome to the About Us page of our simple website.</p>
+      </header>
+    </div>
+  );
+}
+
+export default AboutUs;
